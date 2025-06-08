3ffc5a1a1e31b7f625e148b5ae06fe5cf3c80007
Aditya Vashisht <avdelhistar@gmail.com>
2025-06-09 01:20:42 +0530

    Update urls.py

--- backend_demo/api/urls.py
+++ backend_demo/api/urls.py
@@ -4,3 +4,14 @@ from . import views
 urlpatterns = [
     path('scan-barcode/', views.ScanBarcodeView.as_view(), name='scan-barcode'),
 ]
+
+
+//new code for api integration
+
+from django.urls import path
+from .views import ScanBarcodeView
+
+urlpatterns = [
+    path('scan-barcode/', ScanBarcodeView.as_view(), name='scan-barcode'),
+]
+
