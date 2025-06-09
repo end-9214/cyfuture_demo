ef4bb872a62f7f7ffcfc52bd676d2224e40322d8
Karamveer Singh <karamveersingh2003111@Gmail.com>
2025-06-09 10:20:55 +0530

    Update settings.py

--- backend_demo/backend_demo/settings.py
+++ backend_demo/backend_demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-71w+sxu+8#cd+08518$sh6o9qs!%8+j4g8a_3x$me*wd5m5w=x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -122,3 +122,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS=True
